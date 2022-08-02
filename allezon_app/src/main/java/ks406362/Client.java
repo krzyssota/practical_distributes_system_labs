38fe4e0ded2e9511b888e1d2be8a76f59899e6e9
sotair <krzys.sota@gmail.com>
2022-08-03 01:25:49 +0200

    events debug

--- allezon_app/src/main/java/ks406362/Client.java
+++ allezon_app/src/main/java/ks406362/Client.java
@@ -2,6 +2,7 @@ package ks406362;
 
 import java.util.*;
 
+import com.google.common.collect.EvictingQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -17,24 +18,22 @@ import ks406362.domain.AggregatesQueryResult;
 import ks406362.domain.UserProfileResult;
 import ks406362.domain.UserTagEvent;
 
-import com.google.common.collect.EvictingQueue;
+
 
 @RestController
 public class Client {
 
     private static final Logger log = LoggerFactory.getLogger(Client.class);
-    private final HashMap<String, Queue<UserTagEvent>> usersViews = new HashMap<>();
-    private final HashMap<String, Queue<UserTagEvent>> usersBuys = new HashMap<>();
+    private final HashMap<String, List<UserTagEvent>> usersViews = new HashMap<>();
+    private final HashMap<String, List<UserTagEvent>> usersBuys = new HashMap<>();
 
-    Queue<UserTagEvent> fifo = EvictingQueue.create(200);
 
     @PostMapping("/user_tags")
     public ResponseEntity<Void> addUserTag(@RequestBody(required = false) UserTagEvent userTag) {
-        //HashMap<String, List<UserTagEvent>> userEvents = userTag.action() == Action.BUY ? usersBuys : usersViews;
-        HashMap<String, Queue<UserTagEvent>> userEvents = userTag.action() == Action.BUY ? usersBuys : usersViews;
+        HashMap<String, List<UserTagEvent>> userEvents = userTag.action() == Action.BUY ? usersBuys : usersViews;
 
         String cookie = userTag.cookie();
-        Queue<UserTagEvent> events = userEvents.get(cookie);
+        List<UserTagEvent> events = userEvents.get(cookie);
         if (events != null) {
             log.debug("adding one more event");
             log.debug(String.valueOf(userTag));
@@ -43,15 +42,15 @@ public class Client {
         } else {
             log.debug("first event of user");
             log.debug(String.valueOf(userTag));
-            //List<UserTagEvent> newEvents = new LinkedList<>(Collections.singleton(userTag));
-            Queue<UserTagEvent> newEvents = EvictingQueue.create(200);
-            newEvents.add(userTag);
+            List<UserTagEvent> newEvents = new LinkedList<>(Collections.singleton(userTag));
+            //Queue<UserTagEvent> newEvents = EvictingQueue.create(200);
+            //newEvents.add(userTag);
             userEvents.put(cookie, newEvents);
         }
 
         // debug
-        Queue<UserTagEvent> buys = usersBuys.get(cookie);
-        Queue<UserTagEvent> views = usersViews.get(cookie);
+        List<UserTagEvent> buys = usersBuys.get(cookie);
+        List<UserTagEvent> views = usersViews.get(cookie);
 
         if (buys != null) {
             log.debug("buys");
@@ -73,10 +72,10 @@ public class Client {
             @RequestParam(defaultValue = "200") int limit,
             @RequestBody(required = false) UserProfileResult expectedResult) {
 
-        Queue<UserTagEvent> qBuys = usersBuys.get(cookie);
+        List<UserTagEvent> qBuys = usersBuys.get(cookie);
         List<UserTagEvent> buys = qBuys == null ? new LinkedList<>() : qBuys.stream().toList();
 
-        Queue<UserTagEvent> qViews = usersViews.get(cookie);
+        List<UserTagEvent> qViews = usersViews.get(cookie);
         List<UserTagEvent> views = qViews == null ? new LinkedList<>() : qViews.stream().toList();
 
         UserProfileResult result = new UserProfileResult(cookie, views, buys);
