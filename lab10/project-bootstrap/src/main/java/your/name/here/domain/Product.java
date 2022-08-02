85aa31daa95077c3f8ca1e8149b2abd800d78364
sotair <krzys.sota@gmail.com>
2022-08-02 12:59:35 +0200

    changed rest of the records to classes

--- lab10/project-bootstrap/src/main/java/your/name/here/domain/Product.java
+++ lab10/project-bootstrap/src/main/java/your/name/here/domain/Product.java
@@ -2,8 +2,69 @@ package your.name.here.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record Product(@JsonProperty("product_id") int productId,
-                      @JsonProperty("brand_id") String brandId,
-                      @JsonProperty("category_id") String categoryId,
-                      @JsonProperty("price") int price) {
+public final class Product {
+    @JsonProperty("product_id")
+    private final int productId;
+    @JsonProperty("brand_id")
+    private final String brandId;
+    @JsonProperty("category_id")
+    private final String categoryId;
+    @JsonProperty("price")
+    private final int price;
+
+    public Product(@JsonProperty("product_id") int productId,
+                   @JsonProperty("brand_id") String brandId,
+                   @JsonProperty("category_id") String categoryId,
+                   @JsonProperty("price") int price) {
+        this.productId = productId;
+        this.brandId = brandId;
+        this.categoryId = categoryId;
+        this.price = price;
+    }
+
+    @JsonProperty("product_id")
+    public int productId() {
+        return productId;
+    }
+
+    @JsonProperty("brand_id")
+    public String brandId() {
+        return brandId;
+    }
+
+    @JsonProperty("category_id")
+    public String categoryId() {
+        return categoryId;
+    }
+
+    @JsonProperty("price")
+    public int price() {
+        return price;
+    }
+
+    @java.lang.Override
+    public boolean equals(java.lang.Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Product) obj;
+        return this.productId == that.productId &&
+                java.util.Objects.equals(this.brandId, that.brandId) &&
+                java.util.Objects.equals(this.categoryId, that.categoryId) &&
+                this.price == that.price;
+    }
+
+    @java.lang.Override
+    public int hashCode() {
+        return java.util.Objects.hash(productId, brandId, categoryId, price);
+    }
+
+    @java.lang.Override
+    public String toString() {
+        return "Product[" +
+                "productId=" + productId + ", " +
+                "brandId=" + brandId + ", " +
+                "categoryId=" + categoryId + ", " +
+                "price=" + price + ']';
+    }
+
 }
--- lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
+++ lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
@@ -8,7 +8,86 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 //@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 // We have to use @JsonProperty annotations instead the above due to this bug:
 // https://github.com/FasterXML/jackson-databind/issues/3102
-public record UserTagEvent(Instant time, String cookie, String country, Device device, Action action,
-                           String origin, @JsonProperty("product_info") Product productInfo) {
+public final class UserTagEvent {
+    private final Instant time;
+    private final String cookie;
+    private final String country;
+    private final Device device;
+    private final Action action;
+    private final String origin;
+    @JsonProperty("product_info")
+    private final Product productInfo;
+
+    public UserTagEvent(Instant time, String cookie, String country, Device device, Action action,
+                        String origin, @JsonProperty("product_info") Product productInfo) {
+        this.time = time;
+        this.cookie = cookie;
+        this.country = country;
+        this.device = device;
+        this.action = action;
+        this.origin = origin;
+        this.productInfo = productInfo;
+    }
+
+    public Instant time() {
+        return time;
+    }
+
+    public String cookie() {
+        return cookie;
+    }
+
+    public String country() {
+        return country;
+    }
+
+    public Device device() {
+        return device;
+    }
+
+    public Action action() {
+        return action;
+    }
+
+    public String origin() {
+        return origin;
+    }
+
+    @JsonProperty("product_info")
+    public Product productInfo() {
+        return productInfo;
+    }
+
+    @java.lang.Override
+    public boolean equals(java.lang.Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (UserTagEvent) obj;
+        return java.util.Objects.equals(this.time, that.time) &&
+                java.util.Objects.equals(this.cookie, that.cookie) &&
+                java.util.Objects.equals(this.country, that.country) &&
+                java.util.Objects.equals(this.device, that.device) &&
+                java.util.Objects.equals(this.action, that.action) &&
+                java.util.Objects.equals(this.origin, that.origin) &&
+                java.util.Objects.equals(this.productInfo, that.productInfo);
+    }
+
+    @java.lang.Override
+    public int hashCode() {
+        return java.util.Objects.hash(time, cookie, country, device, action, origin, productInfo);
+    }
+
+    @java.lang.Override
+    public String toString() {
+        return "UserTagEvent[" +
+                "time=" + time + ", " +
+                "cookie=" + cookie + ", " +
+                "country=" + country + ", " +
+                "device=" + device + ", " +
+                "action=" + action + ", " +
+                "origin=" + origin + ", " +
+                "productInfo=" + productInfo + ']';
+    }
+
 
 }
