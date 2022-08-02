82cee91592b6a6f161f66a576608f59e706e54ef
sotair <krzys.sota@gmail.com>
2022-08-02 20:34:55 +0200

    delete comment

--- lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
+++ lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
@@ -12,23 +12,3 @@ public record UserTagEvent(Instant time, String cookie, String country, Device d
                            String origin, @JsonProperty("product_info") Product productInfo) {
 
 }
-
-/*
-2022-08-02 12:36:28,902 [qtp238357312-20] WARN  o.s.h.c.j.MappingJackson2HttpMessageConverter -
-Failed to evaluate Jackson deserialization for type [[simple type, class your.name.here.domain.UserTagEvent]]:
-com.fasterxml.jackson.databind.exc.InvalidDefinitionException:
-Invalid type definition for type `your.name.here.domain.UserTagEvent`:
-Argument #0 of constructor [constructor for `your.name.here.domain.UserTagEvent` (7 args),
-annotations: [null] has no property name annotation; must have name when multiple-parameter constructor annotated as Creator
-
-
-public final class UserTagEvent {
-    private final Instant time;
-    private final String cookie;
-    private final String country;
-    private final Device device;
-    private final Action action;
-    private final String origin;
-    @JsonProperty("product_info")
-    private final Product productInfo;
-* */
