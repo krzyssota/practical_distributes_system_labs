8ddedd334f3af23609ca275b11fbc694139d7a96
sotair <krzys.sota@gmail.com>
2022-08-02 14:45:25 +0200

    back to record

--- lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
+++ lab10/project-bootstrap/src/main/java/your/name/here/domain/UserTagEvent.java
@@ -8,7 +8,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 //@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 // We have to use @JsonProperty annotations instead the above due to this bug:
 // https://github.com/FasterXML/jackson-databind/issues/3102
+public record UserTagEvent(Instant time, String cookie, String country, Device device, Action action,
+                           String origin, @JsonProperty("product_info") Product productInfo) {
 
+}
 
 /*
 2022-08-02 12:36:28,902 [qtp238357312-20] WARN  o.s.h.c.j.MappingJackson2HttpMessageConverter -
@@ -17,7 +20,7 @@ com.fasterxml.jackson.databind.exc.InvalidDefinitionException:
 Invalid type definition for type `your.name.here.domain.UserTagEvent`:
 Argument #0 of constructor [constructor for `your.name.here.domain.UserTagEvent` (7 args),
 annotations: [null] has no property name annotation; must have name when multiple-parameter constructor annotated as Creator
-* */
+
 
 public final class UserTagEvent {
     private final Instant time;
@@ -29,7 +32,7 @@ public final class UserTagEvent {
     @JsonProperty("product_info")
     private final Product productInfo;
 
-    public UserTagEvent(@JsonProperty("time") Instant time, String cookie, String country, Device device, Action action,
+    public UserTagEvent(Instant time, String cookie, String country, Device device, Action action,
                         String origin, @JsonProperty("product_info") Product productInfo) {
         this.time = time;
         this.cookie = cookie;
@@ -102,3 +105,4 @@ public final class UserTagEvent {
 
 
 }
+* */
